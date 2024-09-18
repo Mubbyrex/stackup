ee0846c78125cecd0ba20562f341361f7ea81b98
Mubarak <mubbyre6xx@gmail.com>
2024-09-18 05:37:03 +0000

    new

--- simple-http/target/debug/deps/simple_http-0f21b1b04ad385ea.d
+++
@@ -1,5 +0,0 @@
-/workspaces/stackup/simple-http/target/debug/deps/libsimple_http-0f21b1b04ad385ea.rmeta: src/main.rs
-
-/workspaces/stackup/simple-http/target/debug/deps/simple_http-0f21b1b04ad385ea.d: src/main.rs
-
-src/main.rs:
